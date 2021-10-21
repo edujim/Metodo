48f6004f31e893c37d3669381661b9b078a481f6
Eduardo Jiménez <67086521+edujim@users.noreply.github.com>
2021-10-21 16:14:15 -0600

    Add files via upload

---
+++ src/metodo/MetodoSRSP.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MetodoSRSP {
+    public void Operar(){
+        int a,b,suma;
+        a = 8;
+        b = 10;
+        suma = a+b;
+        
+        System.out.println("El valor de la suma es:" + suma);
+        }
+    
+
+}
